7ea27649688f80b9e51226bc67d26e1546cf9640
Evoldt123 <dannyt2469@gmail.com>
2024-06-03 18:10:13 -0400

    Chat
    Chat does this screen size fit on my laptop

--- poker.py
+++ poker.py
@@ -5,6 +5,7 @@ import pygame
 from pygame.locals import QUIT
 from itertools import combinations 
 
+print()
 
 debug_symbols = {
     "S": "♠",
@@ -25,18 +26,17 @@ class Card():
 value_list = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 suit_list = ["C", "H", "S", "D"]
 hand_values = {
-    1: "Royal Flush",
-    2: "Straight Flush",
-    3: "Four of a Kind",
-    4: "Full House",
-    5: "Flush",
-    6: "Straight",
-    7: "Three of a Kind",
-    8: "Two Pair",
-    9: "One Pair",
-    10: "High Card"
+    10: "Royal Flush",
+    9: "Straight Flush",
+    8: "Four of a Kind",
+    7: "Full House",
+    6: "Flush",
+    5: "Straight",
+    4: "Three of a Kind",
+    3: "Two Pair",
+    2: "One Pair",
+    1: "High Card"
 }
-
 
 def make_deck():
     deck = []
@@ -91,36 +91,36 @@ def calculate_hand(hand):
     # First num is hand type :P
     # TRUMPS
     if straight and flush and arr_rep[-1] > 0:
-        return [1] 
+        return [10] 
     # High Card on Straight
     if straight and flush:
-        return [2, straight_high]
+        return [9, straight_high]
     # Quad, then kicker
     if quads:
-        return [3]
+        return [8]
     # Trip, then Doub
     if trips == 1 and pairs == 1:
-        return [4]
+        return [7]
     # Hand Sorted
     if flush:
         temp_lst = sorted([value_list.index(card.value) for card in hand], reverse=True)
-        fin = [5] + temp_lst
+        fin = [6] + temp_lst
         return fin
     # High Card
     if straight:
-        return [6, straight_high]
+        return [5, straight_high]
     # Trips, Rest
     if trips == 1:
-        return [7]
+        return [4]
         tmp_list = [7, arr_rep.find(3)]
     # Doub, Doub, Rest
     if pairs == 2:
-        return [8]
+        return [3]
     # Doub, Rest
     if pairs == 1:
-        return [9]
+        return [2]
     else:
-        return [10]
+        return [1]
     
 def play_hand():
     def debug_round(river, hand):
@@ -143,17 +143,17 @@ def play_hand():
     for card in hand:
         round_deck.remove(card)
 
-    best = 10
+    best = 0
     full_hand = hand + river
-    #TODO Use itertools smh
+
     combinations_of_5 = combinations(full_hand, 5)
     for combo in combinations_of_5:
-        best = min(calculate_hand(combo)[0], best)
+        best = max(calculate_hand(combo)[0], best)
 
     # best = calculate_hand(river)
     
     debug_round(river, hand)
-    print(f"BEST: {best}.")
+    print(f"BEST: {hand_values[best]}.")
 
 
 for x in range(1):
@@ -164,7 +164,7 @@ for x in range(1):
 
 pygame.init()
 # Set screen size
-screen_size = [600, 600]
+screen_size = [1000, 800]
 screen_w = screen_size[0]
 screen_h = screen_size[1]
 screen = pygame.display.set_mode(screen_size)
@@ -178,6 +178,7 @@ BLUE = (0, 0, 255)
 WHITE = (245, 245, 245)
 BLACK = (0, 0, 0)
 
+LIGHT_BLUE = (127, 183, 240)
 DARK_GREEN = (0, 128, 0)
 YELLOW = (255, 255, 0)
 ORANGE = (255, 165, 0)
@@ -208,3 +209,8 @@ while running:
         # Update window title with current FPS
         pygame.display.set_caption(f'FPS: {round(clock.get_fps())}')
         currentFPS = round(clock.get_fps())
+
+
+
+    screen.fill(LIGHT_BLUE)
+    pygame.display.update()
